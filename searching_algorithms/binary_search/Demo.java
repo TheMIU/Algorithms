5303afd4ffdd22a024b18baf9e3f60e19497cf84
Kasun Miuranga <kasunmiuranga2000@gmail.com>
2023-12-02 13:18:47 +0530

    update

--- searching_algorithms/binary_search/Demo.java
+++ searching_algorithms/binary_search/Demo.java
@@ -2,6 +2,7 @@ package searching_algorithms.binary_search;
 
 public class Demo {
     public static void main(String[] args) {
+        // need a sorted array
         int[] arr = { 2, 3, 4, 10, 40 };
         int target = 10;
         int result = BinarySearch.binarySearch(arr, target);
--- searching_algorithms/linear_search/Demo.java
+++ searching_algorithms/linear_search/Demo.java
@@ -2,6 +2,7 @@ package searching_algorithms.linear_search;
 
 public class Demo {
     public static void main(String[] args) {
+        // need a sorted array
         int[] arr = { 2, 3, 4, 10, 40 };
         int target = 10;
         int result = LinearSearch.linearSearch(arr, target);
