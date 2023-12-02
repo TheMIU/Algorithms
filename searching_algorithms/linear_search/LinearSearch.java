1a1d83f8ba7666cd4e628f7f6ccdd276b009382c
Kasun Miuranga <kasunmiuranga2000@gmail.com>
2023-12-02 12:58:05 +0530

    Linear Search

---
+++ searching_algorithms/linear_search/LinearSearch.java
@@ -0,0 +1,12 @@
+package searching_algorithms.linear_search;
+
+public class LinearSearch {
+    public static int linearSearch(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i; // Return the index if found
+            }
+        }
+        return -1; // Return -1 if not found
+    }
+}
